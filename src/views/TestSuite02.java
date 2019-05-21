d0b3c2b2dd9107ecfdb24d7f03b4cda9c5300eeb
Umar Hambaran <omar.hambaran@frogasia.com>
2019-05-21 13:46:02 +0800

    added sliding windows

--- src/views/TestSuite02.java
+++ src/views/TestSuite02.java
@@ -27,101 +27,141 @@ public class TestSuite02 extends javax.swing.JFrame {
     public TestSuite02() {
         initComponents();
         
-        TestSuiteController.clearSimpleParents();
-        
-        ArrayList<Properties> arrProps = TestSuiteController.parents;
-        for (int i = 0; i < arrProps.size(); i++) {
-            
-            ArrayList<Object> singleParent = new ArrayList<Object>();
-            
-            String strNumberOfPath = "0.00";
-            try {
-                float numberOfPath = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_NUMBER_PATH));
-                strNumberOfPath = Func.df.format(numberOfPath);
-            } catch (Exception e) {
-            }
-            
-            String strExecTime = "0.00";
-            try {
-                float execTime = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_TIME_EXECUTION));
-                strExecTime = Func.df.format(execTime);
-            } catch (Exception e) {
-            }
-            
-            String strTransCoverage = "0.00";
-            try {
-                float transCoverage = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_TRANS_COVERAGE));
-                strTransCoverage = Func.df.format(transCoverage);
-            } catch (Exception e) {
-            }
-            
-            String strTransPairCoverage = "0.00";
-            try {
-                float transPairCoverage = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_TRANS_PAIR_COVERAGE));
-                strTransPairCoverage = Func.df.format(transPairCoverage);
-            } catch (Exception e) {
-            }
-            
-            singleParent.add(strNumberOfPath);
-            singleParent.add(strExecTime);
-            singleParent.add(strTransCoverage);
-            singleParent.add(strTransPairCoverage);
-
-            singleParent.add(arrProps.get(i).get(Func.ARR_PATHS));
-
-            singleParent.add(Integer.MAX_VALUE);
-            
-            TestSuiteController.simpleParents.add(singleParent);
-        }
-        
-        for (int i = 0; i < TestSuiteController.simpleParents.size(); i++) {
-            ArrayList<Object> singleParent = TestSuiteController.simpleParents.get(i);
-            
-            ArrayList<Object> singleParent2 = new ArrayList<Object>();
-            singleParent2.addAll(singleParent);
-            ArrayList<ArrayList<Integer>> oldArr2 = (ArrayList<ArrayList<Integer>>) singleParent2.get(4);
-            ArrayList<ArrayList<Integer>> newArr2 = new ArrayList<ArrayList<Integer>>();
-            for (int j = 0; j < oldArr2.size(); j++) {
-                ArrayList<Integer> newArrDetail = new ArrayList<Integer>();
-                for (int k = 0; k < oldArr2.get(j).size(); k++) {
-                    newArrDetail.add(oldArr2.get(j).get(k));
-                }
-                newArr2.add(newArrDetail);
-            }
-            singleParent2.set(4, newArr2);
-            TestSuiteController.simpleParents2.add(singleParent2);
-            
-            ArrayList<Object> singleParent3 = new ArrayList<Object>();
-            singleParent3.addAll(singleParent);
-            ArrayList<ArrayList<Integer>> oldArr3 = (ArrayList<ArrayList<Integer>>) singleParent3.get(4);
-            ArrayList<ArrayList<Integer>> newArr3 = new ArrayList<ArrayList<Integer>>();
-            for (int j = 0; j < oldArr3.size(); j++) {
-                ArrayList<Integer> newArrDetail = new ArrayList<Integer>();
-                for (int k = 0; k < oldArr3.get(j).size(); k++) {
-                    newArrDetail.add(oldArr3.get(j).get(k));
-                }
-                newArr3.add(newArrDetail);
-            }
-            singleParent3.set(4, newArr3);
-            TestSuiteController.simpleParents3.add(singleParent3);
-        }
-        
         String output = "FIR value:\n";
         
-        // run NSGA2 process
-        long timeNSGA = NSGA2Algo.mainProcess();
+        int numberLoop = 4;
+        ArrayList<ArrayList<Object>> slidingWindows = new ArrayList<ArrayList<Object>>();
         
-        // run SPEA2 process
-        long timeSPEA = SPEA2Algo.mainProcess();
+        for (int w = 0; w < numberLoop; w++) {
+            
+            TestSuiteController.clearSimpleParents();
+
+            ArrayList<Properties> arrProps = TestSuiteController.parents;
+            for (int i = 0; i < arrProps.size(); i++) {
+
+                ArrayList<Object> singleParent = new ArrayList<Object>();
+
+                String strNumberOfPath = "0.00";
+                try {
+                    float numberOfPath = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_NUMBER_PATH));
+                    strNumberOfPath = Func.df.format(numberOfPath);
+                } catch (Exception e) {
+                }
+
+                String strExecTime = "0.00";
+                try {
+                    float execTime = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_TIME_EXECUTION));
+                    strExecTime = Func.df.format(execTime);
+                } catch (Exception e) {
+                }
+
+                String strTransCoverage = "0.00";
+                try {
+                    float transCoverage = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_TRANS_COVERAGE));
+                    strTransCoverage = Func.df.format(transCoverage);
+                } catch (Exception e) {
+                }
+
+                String strTransPairCoverage = "0.00";
+                try {
+                    float transPairCoverage = Float.parseFloat((String) arrProps.get(i).get(Func.TOTAL_TRANS_PAIR_COVERAGE));
+                    strTransPairCoverage = Func.df.format(transPairCoverage);
+                } catch (Exception e) {
+                }
+
+                singleParent.add(strNumberOfPath);
+                singleParent.add(strExecTime);
+                singleParent.add(strTransCoverage);
+                singleParent.add(strTransPairCoverage);
+
+                singleParent.add(arrProps.get(i).get(Func.ARR_PATHS));
+
+                singleParent.add(Integer.MAX_VALUE);
+
+                TestSuiteController.simpleParents.add(singleParent);
+            }
+
+            for (int i = 0; i < TestSuiteController.simpleParents.size(); i++) {
+                ArrayList<Object> singleParent = TestSuiteController.simpleParents.get(i);
+
+                ArrayList<Object> singleParent2 = new ArrayList<Object>();
+                singleParent2.addAll(singleParent);
+                ArrayList<ArrayList<Integer>> oldArr2 = (ArrayList<ArrayList<Integer>>) singleParent2.get(4);
+                ArrayList<ArrayList<Integer>> newArr2 = new ArrayList<ArrayList<Integer>>();
+                for (int j = 0; j < oldArr2.size(); j++) {
+                    ArrayList<Integer> newArrDetail = new ArrayList<Integer>();
+                    for (int k = 0; k < oldArr2.get(j).size(); k++) {
+                        newArrDetail.add(oldArr2.get(j).get(k));
+                    }
+                    newArr2.add(newArrDetail);
+                }
+                singleParent2.set(4, newArr2);
+                TestSuiteController.simpleParents2.add(singleParent2);
+
+                ArrayList<Object> singleParent3 = new ArrayList<Object>();
+                singleParent3.addAll(singleParent);
+                ArrayList<ArrayList<Integer>> oldArr3 = (ArrayList<ArrayList<Integer>>) singleParent3.get(4);
+                ArrayList<ArrayList<Integer>> newArr3 = new ArrayList<ArrayList<Integer>>();
+                for (int j = 0; j < oldArr3.size(); j++) {
+                    ArrayList<Integer> newArrDetail = new ArrayList<Integer>();
+                    for (int k = 0; k < oldArr3.get(j).size(); k++) {
+                        newArrDetail.add(oldArr3.get(j).get(k));
+                    }
+                    newArr3.add(newArrDetail);
+                }
+                singleParent3.set(4, newArr3);
+                TestSuiteController.simpleParents3.add(singleParent3);
+            }
+
+            // run NSGA2 process
+            long timeNSGA = NSGA2Algo.mainProcess();
+
+            // run SPEA2 process
+            long timeSPEA = SPEA2Algo.mainProcess();
+
+            // run PSO process
+            long timePSO = PSOAlgo.mainProcess();
+            
+            output += "\nLoop #"+(w+1)+"\n";
+            output += "NSGA2: (" + NSGA2Algo.numPathParents + " - " + NSGA2Algo.numPathOffsprings + ") / " + NSGA2Algo.numPathParents + " = " + NSGA2Algo.valueFIR + " [" + (timeNSGA * 1.0 / 1000) + "s]\n";
+            output += "SPEA2: (" + SPEA2Algo.numPathParents + " - " + SPEA2Algo.numPathOffsprings + ") / " + SPEA2Algo.numPathParents + " = " + SPEA2Algo.valueFIR + " [" + (timeSPEA * 1.0 / 1000) + "s]\n";
+            output += "PSO  : (" + PSOAlgo.numPathParents + " - " + PSOAlgo.numPathOffsprings + ") / " + PSOAlgo.numPathParents + " = " + PSOAlgo.valueFIR + " [" + (timePSO * 1.0 / 1000) + "s]\n";
+            
+            ArrayList<Object> slidingWindowNSGA2 = new ArrayList<Object>();
+            slidingWindowNSGA2.add("NSGA2");
+            slidingWindowNSGA2.add(NSGA2Algo.valueFIR);
+            slidingWindows.add(slidingWindowNSGA2);
+            
+            ArrayList<Object> slidingWindowSPEA2 = new ArrayList<Object>();
+            slidingWindowSPEA2.add("SPEA2");
+            slidingWindowSPEA2.add(SPEA2Algo.valueFIR);
+            slidingWindows.add(slidingWindowSPEA2);
+            
+            ArrayList<Object> slidingWindowPSO = new ArrayList<Object>();
+            slidingWindowPSO.add("PSO");
+            slidingWindowPSO.add(PSOAlgo.valueFIR);
+            slidingWindows.add(slidingWindowPSO);
+        }
         
-        // run PSO process
-        long timePSO = PSOAlgo.mainProcess();
+        // sort sliding windows from small to big.
+        slidingWindows = TestSuite02.sortSlidingWindows(slidingWindows);
         
-        output += "NSGA2: (" + NSGA2Algo.numPathParents + " - " + NSGA2Algo.numPathOffsprings + ") / " + NSGA2Algo.numPathParents + " = " + NSGA2Algo.valueFIR + " ["+(timeNSGA*1.0/1000)+"s]\n";
-        output += "SPEA2: (" + SPEA2Algo.numPathParents + " - " + SPEA2Algo.numPathOffsprings + ") / " + SPEA2Algo.numPathParents + " = " + SPEA2Algo.valueFIR + " ["+(timeSPEA*1.0/1000)+"s]\n";
-        output += "PSO  : (" + PSOAlgo.numPathParents + " - " + PSOAlgo.numPathOffsprings + ") / " + PSOAlgo.numPathParents + " = " + PSOAlgo.valueFIR + " ["+(timePSO*1.0/1000)+"s]\n";
+        System.out.println("\nSliding windows:");
+        for (int i = 0; i < slidingWindows.size(); i++) {
+            System.out.println(slidingWindows.get(i));
+        }
         
         txtMainScreen.setText(output);
+    }
+    
+    private static ArrayList<ArrayList<Object>> sortSlidingWindows(ArrayList<ArrayList<Object>> sw) {
+        Collections.sort(sw, new Comparator<ArrayList<Object>>() {
+            @Override
+            public int compare(ArrayList<Object> o1, ArrayList<Object> o2) {
+                return Float.parseFloat(o1.get(1).toString()) > Float.parseFloat(o2.get(1).toString()) ? 1 : -1;
+            }
+        });
+        return sw;
     }
     
     public static void viewText(boolean isClear, String text) {
